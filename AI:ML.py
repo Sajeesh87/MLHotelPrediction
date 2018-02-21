270c4325aaf2f78bb75a2935fdfeaa03034f7143
Eshwar <eshwar.mr@elt-114.estblr.com>
2018-02-21 11:15:34 +0530

    commited changes

--- AI:ML.py
+++ AI:ML.py
@@ -1,5 +1,6 @@
 #import libraries
 import pandas as pd
+#import numpy as np
 
 #importing dataset
 dataset_Destination = pd.read_csv('destinations.csv')
@@ -8,23 +9,26 @@ dataset_train = pd.read_csv('test.csv')
 dataset_test = pd.read_csv('train.csv')
 
 #split dependent and indepandent data
-X = dataset_train.iloc[:, :17].values
-y = dataset_train.iloc[:, 17:].values
-X_test = dataset_test.iloc[:, :17].values
+X = dataset_train.iloc[:30000, 0:17]
+y = dataset_train.iloc[:30000, 17:].values
+X_test = dataset_test.iloc[:, 0:17]
 y_test = dataset_test.iloc[:, 17:].values
 
 #categorical data
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder#,OneHotEncoder
 LEX_obj = LabelEncoder()
-X[:,1] = LEX_obj.fit_transform(X[:,1])
-X[:,12] = LEX_obj.fit_transform(X[:,12].astype(str))
-X[:,13] = LEX_obj.fit_transform(X[:,13].astype(str))
-''' no need to split categorical data hence commented
-OHE_obj = OneHotEncoder(categorical_features=[0])
-X = OHE_obj.fit_transform(X).toarray()
-LE_obj = LabelEncoder()
-y = LE_obj.fit_transform(y)
-'''
+for col in X.columns:
+    X[col] = LEX_obj.fit_transform(X[col].astype(str))
+for col in X_test.columns:
+    X_test[col] = LEX_obj.fit_transform(X_test[col].astype(str))
+    
+X = X.values
+X_test = X_test.values
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
 
 #missing data
 from sklearn.preprocessing import Imputer
@@ -36,33 +40,15 @@ X = imputer_obj.transform(X)
 seed = 7
 
 #import algo
-import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold, cross_val_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
 #from sklearn.naive_bayes import GaussianNB
 # prepare models
 models = []
 #models.append(('NB', GaussianNB()))
 models.append(('CART', DecisionTreeClassifier()))
-models.append(('KNN', KNeighborsClassifier()))
 
 # evaluate each model in turn
-results = []
-names = []
-scoring = 'accuracy'
-for name, model in models:
-    print(name)
-    kfold = KFold(n_splits=10, random_state=seed)
-    cv_results = cross_val_score(model, X, y, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
-# boxplot algorithm comparison
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(111)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
+for name,model in models:
+    m = model
+    m.fit(X[:2000],y[:2000])
+    z = m.predict(X_test[:1])
